dde3a0360ee90c4a5d204f6043e1fee199b36ce9
Liza Nesterenko <EYuNesterenko@yandex.ru>
2024-12-24 21:39:09 +0500

    tol

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -26,8 +26,7 @@ urlpatterns = [
 
 if settings.DEBUG:
     import debug_toolbar
-
-    # Добавить к списку urlpatterns список адресов из приложения debug_toolbar:
+    
     urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
